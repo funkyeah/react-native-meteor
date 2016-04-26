26779eac4e280f9104cd7bc2b78421079c4e0c0f
funkyeah <funkyeah@gmail.com>
2016-04-26 02:47:58 -0500

    initial working version of fork with upgrades

--- example/RNApp/app/connect.js
+++ example/RNApp/app/connect.js
@@ -1,6 +1,8 @@
 import Meteor from 'react-native-meteor';
 
 export default function() {
-    const url = 'http://localhost:3000/websocket';
+    //const url = 'http://192.168.56.1:3000/websocket';
+    //const url = 'http://localhost:3000/websocket';
+    const url = 'http://192.168.1.117:3000/websocket';
     Meteor.connect(url);
 }
